c27aaaf1ac54fd650be1f2adde8898992fca31a5
soi-ha <soy2302ten@gmail.com>
2022-01-14 15:56:42 +0900

    solve

--- week2/ba2750.py
+++ week2/ba2750.py
@@ -1,1 +1,22 @@
 # 수 정렬하기
+
+n = int(input())
+li = [] # 수를 담을 리스트
+
+for i in range(n):
+  x = int(input())
+  li.append(x)
+
+li.sort()
+
+print('\n'.join(map(str,li)))
+
+
+# n = int(input())
+# li= []
+# for _ in range(n):
+#     m = int(input())
+#     li.append(m)
+# li.sort()
+
+# print(*li)
