ea068c3fe89224d241b16b7258985b7599b77496
soi-ha <soy2302ten@gmail.com>
2023-11-29 00:09:28 +0900

    8단 변속기: 통과 코드 추가

--- Soofteer_2023/d1_5.py
+++ Soofteer_2023/d1_5.py
@@ -1,6 +1,14 @@
 # 8단 변속기
 # Lv2 
 
+# 성공함
+from sys import stdin
+
+li = list(map(int,stdin.readline().split()))
+
+print("ascending" if li == [1,2,3,4,5,6,7,8] else "descending" if li == [8,7,6,5,4,3,2,1] else "mixed")
+
+# 왜 뭐가 문제인지 안됨
 from sys import stdin
 
 order = str(stdin.readline())
