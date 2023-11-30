6894afa4f827848251d792c1b4852949e9f26699
soi-ha <soy2302ten@gmail.com>
2023-11-30 17:00:31 +0900

    금고털이: 시간초과 해결 코드 추가

--- Soofteer_2023/d1_4.py
+++ Soofteer_2023/d1_4.py
@@ -1,6 +1,32 @@
 # 금고털이
 # Lv.2
 
+## 100.0으로 통과한 코드 
+from sys import stdin
+w, n = map(int, stdin.readline().split())
+mp = []
+
+for i in range(n):
+  mp_value = list(map(int, stdin.readline().split()))
+  mp.append(mp_value)
+
+mp.sort(key=lambda x:-x[1])
+
+result = 0
+add_w = w 
+
+for i in mp:
+  if add_w == 0:
+    break
+  elif i[0] >= add_w:
+    result += add_w * i[1]
+    break  
+  else:
+    result += i[0] * i[1]
+    add_w -= i[0]
+  
+print(result)
+
 # 시간초과 발생
 from sys import stdin
 w, n = map(int, stdin.readline().split())
