cb139458778107c8f792e6d82fc7c75a2f22ace9
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2025-02-24 13:21:35 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.09 ms, Memory: 33.4 MB, Date: 2025.02.24 13:21:31 -BaekjoonHub

---
+++ AlgoPulja_2025/programmers/level0/181928_이어 붙인 수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let odd = ''
+    let even = ''
+    
+    num_list.map(num => {
+        num % 2 ? odd += String(num) : even += String(num)
+    })
+    
+    return Number(odd) + Number(even)
+}
