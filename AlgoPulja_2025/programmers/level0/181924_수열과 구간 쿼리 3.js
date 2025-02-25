19dc250c6148d45229353e55ac7f64c31478bc64
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2025-02-25 12:26:30 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.75 ms, Memory: 33.9 MB, Date: 2025.02.25 12:26:28 -BaekjoonHub

--- AlgoPulja_2025/programmers/level0/181924_수열과 구간 쿼리 3.js
+++ AlgoPulja_2025/programmers/level0/181924_수열과 구간 쿼리 3.js
@@ -1,9 +1,6 @@
 function solution(arr, queries) {
     queries.map(([i,j])=>{
-        const prev_i = arr[i]
-        const prev_j = arr[j]
-        arr[i] = prev_j
-        arr[j] = prev_i
+        [arr[i], arr[j]] = [arr[j], arr[i]]
     })
     return arr
 }
