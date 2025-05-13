77aee5d2d6d140af2bdc5aa0605847ae7f0a9ad5
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2025-05-13 21:22:43 +0900

    [level 1] Title: 삼총사, Time: 0.22 ms, Memory: 33.6 MB, Date: 2025.05.13 21:22:40 -BaekjoonHub

---
+++ AlgoPulja_2025/programmers/level1/131705_삼총사.js
@@ -0,0 +1,15 @@
+function solution(number) {
+  let result = 0;
+  const n = number.length;
+
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          result++;
+        }
+      }
+    }
+  }
+  return result;
+}
